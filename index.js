716b948f431bc30774e9703cbeb0049d8c92fc1c
Gabriel <mercado.gabriel91@hotmail.com>
2020-10-21 19:22:46 -0300

    chore:agregar form

---
+++ index.js
@@ -0,0 +1,22 @@
+// TODO LIST: 
+// Para todas las páginas:
+
+// - Menú con links a todas las páginas, tanto en el encabezado como en el pie de página
+
+// Para el index.html:
+
+// Contenido que corresponda a la portada de un emprendimiento.
+
+// Para galeria.html
+
+// - Usar etiquetas multimedia, tanto img, audio o video
+// Para contacto.html
+
+// - Armar un formulario con nombre, apellido, correo electrónico, mensaje (<textarea>)
+// Para quienes-somos.html
+
+// - Distintos tipos de encabezados
+// - Listas
+// - Hipervínculos
+
+console.log('js linked')
