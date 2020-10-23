dda7635959fcaa573e1b1070a764c2b85594d824
Gabriel <mercado.gabriel91@hotmail.com>
2020-10-23 19:57:18 -0300

    nonsense commit

--- index.js
+++ index.js
@@ -20,3 +20,16 @@
 // - Hipervínculos
 
 console.log('js linked')
+
+// `meta tags for social networks previews
+// egg: Facebook <meta property = "og: url" /> ` 
+
+// Formulario con:
+// - nombre |*MANDATORY | min2 max20 
+// -apellido *MANDATORY | min2 max20 
+// -email *MANDATORY | min6 max225 
+// -DNI / -CUIL *MANDATORY | min8 max11 | Int only
+// -Altura (Cm) *MANDATORY | positive only | de uno en uno 
+// -Peso (Kg)*MANDATORY | positive only | de .1 en 1
+// -Edad | positive only | min14 max100 
+// -Send button
